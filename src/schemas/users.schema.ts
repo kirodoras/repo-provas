e0ea28ad9d98f6ba3bf349f52798f017b9500896
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-14 17:47:37 -0300

    feat: set users schema

---
+++ src/schemas/users.schema.ts
@@ -0,0 +1,15 @@
+import joi from "joi";
+import * as usersTypes from "../types/users.types";
+
+const signUpSchema = joi.object<usersTypes.IUserSingUp>({
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+  confirmPassword: joi.string().required().valid(joi.ref("password")),
+});
+
+const signInSchema = joi.object<usersTypes.TUser>({
+  email: joi.string().email().required(),
+  password: joi.string().min(8).required(),
+});
+
+export { signUpSchema, signInSchema };
