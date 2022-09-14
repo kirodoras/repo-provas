b903ba37c8ef0db9985a905d5ce438b298aede3a
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-14 17:51:35 -0300

    feat: set users controller

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import * as usersServices from "../services/users.services";
+import * as usersTypes from "../types/users.types";
+
+export async function signUp(req: Request, res: Response) {
+  const { email, password } = req.body;
+  const user: usersTypes.TUser = { email, password };
+  await usersServices.signUp(user);
+  res.sendStatus(200);
+}
+
+export async function signIn(req: Request, res: Response) {
+  const { email, password } = req.body;
+  const user: usersTypes.TUser = { email, password };
+  const token = await usersServices.signIn(user);
+  res.send({ token });
+}
