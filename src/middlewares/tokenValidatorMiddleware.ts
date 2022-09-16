ffc08dd4bcad4b67c998263468c61e70c0131884
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-16 16:29:22 -0300

    feat: implement token validator middleware

---
+++ src/middlewares/tokenValidatorMiddleware.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from "express";
+import * as jwtProvider from "../providers/jwtProvider";
+import * as usersServices from "../services/usersService";
+
+type Payload = {
+  email: string;
+  iat: number;
+  exp: number;
+};
+
+export async function tokenValidator(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { authorization } = req.headers;
+  const token: string | undefined = authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    throw { type: "unauthorized", message: "Invalid request" };
+  }
+
+  const payload = jwtProvider.decode(token);
+  const { email } = payload as Payload;
+
+  if (!email) {
+    throw { type: "unauthorized", message: "Invalid request" };
+  }
+
+  const user = await usersServices.checkUserNotExists(email);
+  const user_id: number = +user.id;
+  res.locals.user_id = user_id;
+
+  next();
+}
