8b8e5be596b8ebff758684ab5da587dcd0d80f5d
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-16 16:34:52 -0300

    feat: insert test in tests service

---
+++ src/services/testsService.ts
@@ -0,0 +1,13 @@
+import * as testsRepository from "../repositories/testsRepository";
+import * as categoriesService from "./categoriesService";
+import * as teachersDisciplinesService from "./teachersDisciplinesService";
+import * as testsTypes from "../types/testsTypes";
+
+export async function insert(test: testsTypes.TTest) {
+  const { categoryId, teacherDisciplineId } = test;
+  await categoriesService.checkExistsCategory(categoryId);
+  await teachersDisciplinesService.checkExistsTeacherDiscipline(
+    teacherDisciplineId
+  );
+  await testsRepository.insert(test);
+}
