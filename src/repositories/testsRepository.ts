98bf4f961a185981e1f691511ad2831651b9a9cb
Mateus Figueiredo Pereira <figueiredo1497@gmail.com>
2022-09-17 12:20:46 -0300

    feat: builds query relation between terms and tests

--- src/repositories/testsRepository.ts
+++ src/repositories/testsRepository.ts
@@ -7,3 +7,32 @@ export async function insert(test: testsTypes.TTest) {
   });
   return result;
 }
+
+export async function getByTerms() {
+  const tests: object[] = await client.term.findMany({
+    select: {
+      id: true,
+      number: true,
+      Disciplines: {
+        select: {
+          id: true,
+          name: true,
+          TeacherDiscipline: {
+            select: {
+              Teacher: true,
+              Test: {
+                select: {
+                  id: true,
+                  name: true,
+                  pdfUrl: true,
+                  Category: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  return tests;
+}
